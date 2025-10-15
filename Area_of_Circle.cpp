1e9231d761d992acd39e4562954920204c45dcd7
maazali9559-hash <maazali9559@gamail.com>
2025-10-15 03:48:39 -0700

    Area finding

---
+++ Area_of_Circle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int r;
+    cout<<"enter value :"<<endl;
+    cin>>r;
+    cout<<"AREA :"<<3.14*r*r;
+    return 0;
+}
---
+++ Area_of_rectangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int l,w ;
+    cout<<"enter value:"<<endl;
+    cin>>l>>w;
+    cout<<"AREA:"<<l*w;
+    return 0;
+    
+}

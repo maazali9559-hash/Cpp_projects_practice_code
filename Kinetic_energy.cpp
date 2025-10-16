989edad225cf797c29f00513c64ef46f4aa6a53f
maazali9559-hash <maazali9559@gamail.com>
2025-10-16 06:48:43 -0700

    kinetic energy

---
+++ Kinetic_energy.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,v;
+    cout<<"enter value:"<<endl;
+    cin>>m>>v;
+    float KE = 0.5*m*v*v;
+    cout<<"kinetic energy="<<KE;
+    return 0;
+    
+}

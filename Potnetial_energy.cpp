b083ef751febba202e32fe58bc08c304aad33ba0
maazali9559-hash <maazali9559@gamail.com>
2025-10-16 06:53:31 -0700

    Potential Energy

---
+++ Potnetial_energy.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,g,h;
+    cout<<"Enter your values:"<<endl;
+    cin>>m>>g>>h;
+    float PE = m*g*h;
+    cout<<"Potential Energy="<<PE;
+    return 0;
+    
+
+}

d29649343f658b528999aae47d43e5e0a09b8a19
maazali9559-hash <maazali9559@gamail.com>
2025-10-18 04:22:51 -0700

    if else program for even or odd number

---
+++ if_else_for_even_or_odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int s;
+    cout<<"Enter your number"<<endl;
+    cin>>s;
+    if(s%2==0){
+    cout<<"Even"<<endl;
+    }
+    else{
+    cout<<"Odd"<<endl;
+    }
+    return 0;
+}

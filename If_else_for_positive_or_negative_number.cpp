6bf74f7c09965a784bb2dabf2be0cfd174bf6b12
maazali9559-hash <maazali9559@gamail.com>
2025-10-19 06:25:39 -0700

    if else program for positive or negative number

---
+++ If_else_for_positive_or_negative_number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int j;
+    cout<<"Enter your number:"<<endl;
+    cin>>j;
+    if(j>0){
+        cout<<"Your number is positive"<<endl;
+    }
+    else{
+        cout<<"Your number is negative"<<endl;
+    }
+    return 0;
+}
